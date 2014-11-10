050c927814c82386c482aad22ea2bac5b79ea4a7
Eric Hankins <ehankins@rednovalabs.com>
2014-11-10 14:14:59 -0600

    Update extconf for newer Libre version

--- ext/rubyuno/extconf.rb
+++ ext/rubyuno/extconf.rb
@@ -1,48 +1,64 @@
 require 'mkmf'
 require 'open3'
 
+
+
+################################################################################
+# Configuration
+# Set the paths to these programs on your system if they aren't in your PATH
+################################################################################
+CPPUMAKER = 'cppumaker'
+SETSDKENV = 'setsdkenv_unix'
+################################################################################
+
+
+
+
+
+
+
+
+
+
 # IDL Headers
 IDL_TYPES = [
-  "com.sun.star.beans.MethodConcept", 
-  "com.sun.star.beans.PropertyAttribute", 
-  "com.sun.star.beans.XIntrospection", 
-  "com.sun.star.beans.XIntrospectionAccess", 
-  "com.sun.star.beans.XMaterialHolder", 
-  "com.sun.star.container.XEnumerationAccess", 
-  "com.sun.star.container.XHierarchicalNameAccess", 
-  "com.sun.star.container.XIndexContainer", 
-  "com.sun.star.container.XNameContainer", 
-  "com.sun.star.lang.XMultiServiceFactory", 
-  "com.sun.star.lang.XServiceInfo", 
-  "com.sun.star.lang.XSingleComponentFactory", 
-  "com.sun.star.lang.XSingleServiceFactory", 
-  "com.sun.star.lang.XTypeProvider", 
-  "com.sun.star.lang.XUnoTunnel", 
-  "com.sun.star.reflection.InvocationTargetException", 
-  "com.sun.star.reflection.ParamMode", 
-  "com.sun.star.reflection.XConstantTypeDescription", 
-  "com.sun.star.reflection.XConstantsTypeDescription", 
-  "com.sun.star.reflection.XEnumTypeDescription", 
-  "com.sun.star.reflection.XIdlReflection", 
-  "com.sun.star.reflection.XInterfaceTypeDescription2", 
-  "com.sun.star.reflection.XTypeDescription", 
-  "com.sun.star.registry.XRegistryKey", 
-  "com.sun.star.script.InvocationInfo", 
-  "com.sun.star.script.MemberType", 
-  "com.sun.star.script.XInvocation2", 
-  "com.sun.star.script.XInvocationAdapterFactory2", 
-  "com.sun.star.script.XTypeConverter", 
-  "com.sun.star.script.provider.ScriptFrameworkErrorException", 
-  "com.sun.star.uno.XAggregation", 
-  "com.sun.star.uno.TypeClass", 
-  "com.sun.star.uno.XComponentContext", 
-  "com.sun.star.uno.XWeak", 
+  "com.sun.star.beans.MethodConcept",
+  "com.sun.star.beans.PropertyAttribute",
+  "com.sun.star.beans.XIntrospection",
+  "com.sun.star.beans.XIntrospectionAccess",
+  "com.sun.star.beans.XMaterialHolder",
+  "com.sun.star.container.XEnumerationAccess",
+  "com.sun.star.container.XHierarchicalNameAccess",
+  "com.sun.star.container.XIndexContainer",
+  "com.sun.star.container.XNameContainer",
+  "com.sun.star.lang.XMultiServiceFactory",
+  "com.sun.star.lang.XServiceInfo",
+  "com.sun.star.lang.XSingleComponentFactory",
+  "com.sun.star.lang.XSingleServiceFactory",
+  "com.sun.star.lang.XTypeProvider",
+  "com.sun.star.lang.XUnoTunnel",
+  "com.sun.star.reflection.InvocationTargetException",
+  "com.sun.star.reflection.ParamMode",
+  "com.sun.star.reflection.XConstantTypeDescription",
+  "com.sun.star.reflection.XConstantsTypeDescription",
+  "com.sun.star.reflection.XEnumTypeDescription",
+  "com.sun.star.reflection.XIdlReflection",
+  "com.sun.star.reflection.XInterfaceTypeDescription2",
+  "com.sun.star.reflection.XTypeDescription",
+  "com.sun.star.registry.XRegistryKey",
+  "com.sun.star.script.InvocationInfo",
+  "com.sun.star.script.MemberType",
+  "com.sun.star.script.XInvocation2",
+  "com.sun.star.script.XInvocationAdapterFactory2",
+  "com.sun.star.script.XTypeConverter",
+  "com.sun.star.script.provider.ScriptFrameworkErrorException",
+  "com.sun.star.uno.XAggregation",
+  "com.sun.star.uno.TypeClass",
+  "com.sun.star.uno.XComponentContext",
+  "com.sun.star.uno.XWeak",
 ]
 
 CPPU_INCLUDE = './include'
-CPPUMAKER = 'cppumaker'
-CPPUMAKER_FLAG = "#{CPPU_INCLUDE}/flag"
-SETSDKENV = 'setsdkenv_unix'
 
 def missing(item)
   die "couldn't find #{item} (required)"
@@ -63,17 +79,14 @@ c = RbConfig::CONFIG
 host = c['host']
 
 missing(CPPUMAKER) unless find_executable(CPPUMAKER)
-setsdkenv_path = checking_for checking_message(SETSDKENV) do
-  `find / -name #{SETSDKENV} &2>/dev/null`.chomp
-end
-missing(SETSDKENV) unless setsdkenv_path
+missing(SETSDKENV) unless find_executable(SETSDKENV)
 
 # Collected needed environment variables
 sdk_home = ENV['OO_SDK_HOME']
 ure_home = ENV['OO_SDK_URE_HOME']
-base_path = ENV['OFFICE_BASE_PROGRAM_PATH']
+base_path = ENV['OFFICE_PROGRAM_PATH']
 environment_ready = true
-%w(OO_SDK_HOME OO_SDK_URE_HOME OFFICE_BASE_PROGRAM_PATH).each do |env|
+%w(OO_SDK_HOME OO_SDK_URE_HOME OFFICE_PROGRAM_PATH).each do |env|
   #environment_ready &&= find_environment(env)
   res = find_environment(env)
   environment_ready &&= res
@@ -82,35 +95,31 @@ end
 unless environment_ready
   message "collecting ENV values:\n"
   IO.popen("#{setsdkenv_path} >/dev/null", "w") {} # Ensure the SDK is set up
-  res = Open3.popen3("/opt/openoffice.org/basis3.4/sdk/setsdkenv_unix") do |i, o, e|
+  res = Open3.popen3(setsdkenv_path) do |i, o, e|
     i.puts "echo $OO_SDK_HOME"
     i.puts "echo $OO_SDK_URE_HOME"
-    i.puts "echo $OFFICE_BASE_PROGRAM_PATH"
+    i.puts "echo $OFFICE_PROGRAM_PATH"
     i.close_write
     o.readlines
   end
   sdk_home, ure_home, base_path = res[-5..-2].map(&:chomp)
   message "$OO_SDK_HOME=#{sdk_home}\n"
   message "$OO_SDK_URE_HOME=#{ure_home}\n"
-  message "$OFFICE_BASE_PROGRAM_PATH=#{base_path}\n"
+  message "$OFFICE_PROGRAM_PATH=#{base_path}\n"
 end
 
-unless File.exists?(CPPUMAKER_FLAG)
-  message "generating C++ representation for OpenOffice IDL types... "
-  ure_types = host['linux'] ? "#{ure_home}/share/misc/types.rdb" : "#{ure_home}/misc/types.rdb"
-  office_types = "#{base_path}/offapi.rdb"
-  IO.popen("/opt/openoffice.org/basis3.4/sdk/setsdkenv_unix >/dev/null", "w") do |shell|
-    shell.puts "#{CPPUMAKER} -Gc -BUCR -O#{CPPU_INCLUDE} -T\"#{IDL_TYPES.join(';')}\" \"#{ure_types}\" \"#{office_types}\""
-    shell.puts "touch #{CPPUMAKER_FLAG}"
-  end
-  
-  message "ok\n"
+message "generating C++ representation for OpenOffice IDL types... "
+ure_types = "#{ure_home}/share/misc/types.rdb"
+office_types = "#{base_path}/types/offapi.rdb"
+IO.popen("#{setsdkenv_path} >/dev/null", "w") do |shell|
+  shell.puts "#{CPPUMAKER} -Gc -C -O#{CPPU_INCLUDE} -T\"#{IDL_TYPES.join(';')}\" \"#{ure_types}\" \"#{office_types}\""
 end
+message "ok\n"
 
 if host['mswin']
   sdk_lib = "/LIBPATH:\"#{sdk_home}/lib\""
   ure_lib = ""
-  sdk_libs = " icppuhelper.lib icppu.lib isal.lib isalhelper.lib msvcprt.lib msvcrt.lib kernel32.lib" # 
+  sdk_libs = " icppuhelper.lib icppu.lib isal.lib isalhelper.lib msvcprt.lib msvcrt.lib kernel32.lib" #
 else
   sdk_lib = "-L\"#{sdk_home}/lib\""
   ure_lib = "-L\"#{ure_home}/lib\""
